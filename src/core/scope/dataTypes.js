7b3e2d7159068133ec61d9c13b3c89656439054e
Harshit Gupta <kohthecodemaster@gmail.com>
2023-06-28 14:42:31 +0530

    4th Commit - Tuple, Unknown Data Types & Type Assertion.
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- src/core/scope/dataTypes.js
+++ src/core/scope/dataTypes.js
@@ -11,10 +11,12 @@ GitHub: https://github.com/KOHTheCodeMaster/TypeScript-Fundamentals
 */
 main();
 function main() {
-    availableDataTypes();
-    compileTimeErrorOnChangingDataType();
-    enumGradeExample();
-    enumRomanNumberExample();
+    // availableDataTypes();
+    unknownDataType();
+    tupleDataType();
+    // compileTimeErrorOnChangingDataType();
+    // enumGradeExample();
+    // enumRomanNumberExample();
 }
 function availableDataTypes() {
     // console.log("Method: availableDataTypes Begins.");
@@ -25,6 +27,73 @@ function availableDataTypes() {
     var numArr = [1, 2, 3];
     var anyArr = [1, true, 'XYZ'];
     // console.log("Method: availableDataTypes Ends.\n");
+}
+function unknownDataType() {
+    /*
+        In Typescript, any value can be assigned to unknown. Similar to 'any', we can assign any value to the unknown.
+        The 'unknown' type in TypeScript represents a value that can be of any type. It is similar to 'any' type, but with stricter usage rules.
+        While any allows you to perform any operation on it without type checking, unknown requires you to perform some type checking before operating on it.
+
+        Unknown type is used to make our code type-safe. when we use the unknown type we have to write extra code but finally,
+        our code will be type-safe. Type safety is nothing but the prevention of type errors.
+     */
+    console.log("Method: unknownDataType Begins.");
+    var unknownX;
+    console.log(unknownX);
+    unknownX = true; //  Assigning boolean value
+    console.log(unknownX);
+    unknownX = 1; //  Assigning number value
+    console.log(unknownX);
+    unknownX = "ABC"; //  Assigning string value
+    console.log(unknownX);
+    unknownX = [1, 2, 3, 4]; //  Assigning Array value
+    console.log(unknownX);
+    unknownX = { name: "John Doe" }; //  Assigning object value
+    console.log(unknownX);
+    unknownX = Math.random(); //  Assigning random number value
+    console.log(unknownX);
+    unknownX = null; //  Assigning null value
+    console.log(unknownX);
+    unknownX = undefined; //  Assigning undefined value
+    console.log(unknownX);
+    var str = "ABC";
+    console.log(str);
+    //  When we attempt to perform any operation directly on unknown type variable, we will get compile time error.
+    var strLen1 = str.length; //  TS2339: Property 'length' does not exist on type 'unknown'.
+    console.log('strLen1: ' + strLen1);
+    console.log("\nMethod: unknownDataType Ends.\n");
+}
+function tupleDataType() {
+    /*
+        In the above example, we have defined a variable num1 as number type and str1 as string type with values.
+        The same thing can be achieved by using a single tuple type variable with two values of number and string type.
+        Thus, removing the need to declare two different variables.
+
+        Tuple can be considered as 'Any' Array
+     */
+    console.log("Method: tupleDataType Begins.");
+    var num1 = 1;
+    var str1 = "ABC";
+    var tuple1 = [1, "ABC"]; // Tuple type variable
+    //  A tuple type variable can include multiple data types as well.
+    var employee = [1, "John"];
+    var person = [1, "Doe", true];
+    var user = [1, "Steve", true, 20, "Admin"]; // initialize tuple variable
+    //  We can declare an array of tuple also.
+    var tupleArr = [[1, "A"], [2, "B"], [3, "C"]];
+    //  Accessing Tuple Elements
+    var tuple2 = [1, "XYZ"];
+    tuple2[0]; // returns 1
+    tuple2[1]; // returns "XYZ"
+    //  Add Elements into Tuple
+    var tuple3 = [1, "John"];
+    tuple3.push(2, "Doe");
+    console.log('Tuple3: ' + tuple3);
+    tuple3.push(3, 4, 5, 6, 7); //  This is also valid, not sure why... Suspicious Doubt Unresolved!
+    console.log('Tuple3: ' + tuple3);
+    //  When trying to add invalid data type to tuple, we get compile time error.
+    tuple3.push(true); //  TS2345: Argument of type 'boolean' is not assignable to parameter of type 'string | number'.
+    console.log("\nMethod: tupleDataType Ends.\n");
 }
 function compileTimeErrorOnChangingDataType() {
     console.log("Method: compileTimeErrorOnChangingDataType Begins.\n");
@@ -79,7 +148,7 @@ function enumRomanNumberExample() {
         RomanNumber[RomanNumber["M"] = 1000] = "M";
     })(RomanNumber || (RomanNumber = {}));
     ;
-    console.log('Using enums, it allows to assign only available valid values, ' +
+    console.log('\nUsing enums, it allows to assign only available valid values, ' +
         'and if we try to assign any invalid values, it\'ll give us the Error during compile time itself.');
     var num1 = RomanNumber.I;
     console.log('num1: ' + num1); //  romanNum: 1
